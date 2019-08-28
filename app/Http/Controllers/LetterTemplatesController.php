24f6a42e68a767575361066b517bc492852f6e82
Lucky.JC <lucky.jonathan.lc@gmail.com>
2019-08-28 18:43:08 +0700

    get fields

--- app/Http/Controllers/LetterTemplatesController.php
+++ app/Http/Controllers/LetterTemplatesController.php
@@ -28,27 +28,27 @@ class LetterTemplatesController extends Controller{
                 $temp = explode('_',$field);
                 if(count($temp) == 1){
                     $vars[] = [
-                        "name" => $temp[0];
-                    ]
+                        "name" => $temp[0]
+                    ];
                 }
                 else if(count($temp)==2 && $temp[0]=='datetime'){
                     $vars[] = [
-                        "datetime" => true;
-                        "name" => $temp[0];
-                    ]
+                        "datetime" => true,
+                        "name" => $temp[0]
+                    ];
                 }
                 else if(count($temp)==2){
                     $vars[] = [
-                        "name" => $temp[0];
-                        "desc" => $temp[1];
-                    ]
+                        "name" => $temp[0],
+                        "desc" => $temp[1]
+                    ];
                 }
                 else if(count($temp)==3){
                     $vars[] = [
-                        "datetime" => true;
-                        "name" => $temp[0];
-                        "desc" => $temp[1];
-                    ]
+                        "datetime" => true,
+                        "name" => $temp[1],
+                        "desc" => $temp[2]
+                    ];
                 }
                 else{
                     return response(["status"=>"ERROR","msg"=>"Invalid document."]);
