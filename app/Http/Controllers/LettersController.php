cfab6a89422f2547c1493722e2b698e7ffbad646
unknown <ftnfata@gmail.com>
2019-08-31 11:59:40 +0700

    commit

--- app/Http/Controllers/LettersController.php
+++ app/Http/Controllers/LettersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Letter;
+use App\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\File;
 use Storage;
